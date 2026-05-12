9f78f66f4b3bb3d392ad2778cb134b3071aa55f4
Git-soumik1802 <128280509+Git-soumik1802@users.noreply.github.com>
2026-05-12 16:03:43 +0530

    fixed backend setup

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import "./seed.js";
+
 import connectDB from "./utils/db.js";
 
 import userRoute from "./routes/user.route.js";
@@ -23,16 +23,20 @@ connectDB();
 // ================= MIDDLEWARE =================
 app.use(express.json());
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(cookieParser());
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 // ================= API ROUTES =================
 app.use("/api/v1/user", userRoute);
@@ -43,25 +47,32 @@ app.use("/api/v1/job", jobRoute);
 
 app.use("/api/v1/application", applicationRoute);
 
-// ================= FRONTEND =================
+// ================= TEST ROUTE =================
+app.get("/api/test", (req, res) => {
+  res.send("API Working Successfully");
+});
+
+// ================= FRONTEND STATIC =================
 app.use(
-    express.static(
-        path.join(__dirname, "../frontend/dist")
-    )
+  express.static(
+    path.join(__dirname, "../frontend/dist")
+  )
 );
 
 app.get("*", (req, res) => {
-    res.sendFile(
-        path.join(
-            __dirname,
-            "../frontend/dist/index.html"
-        )
-    );
+  res.sendFile(
+    path.resolve(
+      __dirname,
+      "../frontend",
+      "dist",
+      "index.html"
+    )
+  );
 });
 
 // ================= SERVER =================
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+  console.log(`Server running at port ${PORT}`);
 });
